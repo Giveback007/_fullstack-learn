4fc4081d5503b8237842d1b0022c972e166516bc
Dovy <dovy91@gmail.com>
2024-06-26 23:54:04 -0400

    Implemented tailwind and htmx

--- 2--go-templ-htmx-tailwind-svelte/server/main.go
+++ 2--go-templ-htmx-tailwind-svelte/server/main.go
@@ -1,37 +1,54 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"net/http"
-
-// 	"github.com/a-h/templ"
-// )
-
-// func main() {
-// 	component := hello("John!")
-
-// 	http.Handle("/", templ.Handler(component))
-
-// 	fmt.Println("Listening on http://localhost:8080/")
-// 	http.ListenAndServe(":8080", nil)
-// }
-
 package main
 
 import (
 	"fmt"
+	"full-stack/server/views"
 	"net/http"
+	"strconv"
 
+	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
 )
 
+func MakeNames(names []string) []views.Name {
+	nameArr := []views.Name{}
+	for _, n := range names {
+		nameArr = append(nameArr, views.Name{Name: n})
+	}
+
+	return nameArr
+}
+
+func render(c *gin.Context, status int, template templ.Component) error {
+	c.Status(status)
+	return template.Render(c.Request.Context(), c.Writer)
+}
+
 func main() {
+	count := 0
 	r := gin.Default()
+	r.Static("css", "./server/css")
+
+	r.GET("/", func(c *gin.Context) {
+		names := MakeNames([]string{"dovy", "karen", "angel"})
+
+		render(c, http.StatusOK, views.MakeHomePage(names, strconv.Itoa(count)))
+	})
+
+	r.POST("/count-up", func(c *gin.Context) {
+		count++
+		c.Status(200)
+		c.String(http.StatusOK, strconv.Itoa(count))
+		// ???
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	/* LOCALHOST */
 	fmt.Println("Listening on http://localhost:3000/")
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- 2--go-templ-htmx-tailwind-svelte/tailwind.config.js
+++ 2--go-templ-htmx-tailwind-svelte/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./internal/**/*.{go,js,templ,html,ts,svelte}"
+    "./server/**/*.{go,js,ts,templ,html,svelte}"
   ],
   theme: {
     extend: {},
